38906104e9c46cc46e726faf878f7d9177e53253
Gilang <giirugiru@gmail.com>
2023-06-08 09:29:19 +0700

    Initial Commit

---
+++ PokemonApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonApp
+//
+//  Created by Gilang-M1Pro on 08/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
