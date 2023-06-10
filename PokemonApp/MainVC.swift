83b1cbaec8fec9824ebf8c24d8ea86ac55a2e1df
Gilang <giirugiru@gmail.com>
2023-06-10 10:36:39 +0700

    initialize storyboardless configuration

--- PokemonApp/MainVC.swift
+++ PokemonApp/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  PokemonApp
 //
 //  Created by Gilang-M1Pro on 08/06/23.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .red
     }
-
-
 }
 
